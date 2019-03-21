bf477188882acf33ff4ba26ad7bd8cfcd4da900f
Sarah M Sicard <ssicar1@lsu.edu>
2019-03-21 11:26:58 -0500

    worked on 2 and 3

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -4,16 +4,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
 }).addTo(map);
 
-L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-  layers: 'nexrad-n0r-900913',
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer?request=GetCapabilities&service=WMS', {
+  layers: '1',
   format: 'image/png',
   transparent: true,
-  attribution: 'NOAA, Iowa State University'
+  attribution: 'NOAA'
 }).addTo(map);
 
-L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
-  layers: 'warnings_c',
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/wwa_meteocean_tropicalcyclones_inundation/MapServer/WMSServer', {
+  layers: '1',
   format: 'image/png',
   transparent: true,
-  attribution: 'NOAA, Iowa State University'
+  attribution: 'NOAA'
 }).addTo(map);
