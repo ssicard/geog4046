e43ac55952e539a5667632479363284042d80fab
Sarah M Sicard <ssicar1@lsu.edu>
2019-03-28 11:47:21 -0500

    changed opacity

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,19 +1,20 @@
-let map = L.map('map').setView([32.18, -99.14], 4)
+let weatherAndCoastMap = L.map('map').setView([32.18, -99.14], 4)
 L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-}).addTo(map);
+}).addTo(weatherAndCoastMap);
 
 L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_imagery_time/MapServer/WMSServer', {
   layers: '1',
   format: 'image/png',
   transparent: true,
+	opacity: 0.5,
   attribution: 'NOAA'
-}).addTo(map);
+}).addTo(weatherAndCoastMap);
 
 L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/guidance_model_coastalocean_estofs_time/MapServer/WMSServer', {
   layers: '1',
   format: 'image/png',
   transparent: true,
   attribution: 'NOAA'
-}).addTo(map);
+}).addTo(weatherAndCoastMap);
--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,42 +1,54 @@
 let map = L.map('map').setView([30.4515, -91.1871], 9)
 L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-}).addTo(map);
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+}).addTo(map)
 
-//
 let seniorCentersUrl = 'https://opendata.arcgis.com/datasets/ca5cc72995064330ae19708f2cfaf134_0.geojson'
+
+let test1 = 'try2'
+var greenIcon = L.icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+  // shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41]
+  // iconAnchor: [12, 41],
+  // popupAnchor: [1, -34],
+  // shadowSize: [41, 41]
+})
+var marker = L.marker([30.4515, -91.1871], {icon: greenIcon}).addTo(map);
 
 var centers;
 jQuery.getJSON(seniorCentersUrl, function (data) {
-	let centerStyle = function(feature) {
+  let centerStyle = function (feature) {
+    let city = feature.properties.CITY
+    // console.log('setting center style')
 
-			let greenIcon = new L.Icon({
-				iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
-			});
-			
-		return {
-			icon: greenIcon
-  		// shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-  		// iconSize: [25, 41],
-  		// iconAnchor: [12, 41],
-  		// popupAnchor: [1, -34],
-		  // shadowSize: [41, 41]
-		}
-	}
-	let seniorCenterGeojsonOptions = {
-		style: centerStyle,
-		onEachFeature: createPopup
-	}
-	centers = L.geoJSON(data, seniorCenterGeojsonOptions).addTo(map);
-});
+    if (city === 'BATON ROUGE') {
+      // console.log('trying blue icon')
+      return {
 
-let createPopup = function(feature, layer){
-	let name = feature.properties.NAME;
-	let address = feature.properties.FULL_ADDRESS;
-	let city = feature.properties.CITY;
-	let state = feature.properties.STATE;
-	let zip = feature.properties.ZIP;
-	layer.bindPopup('<b>Senior Center Name: </b>' + name + '<br><br>' +
-		'<b> Address: </b>' + address + ', ' + city + ', ' + state + ', ' + zip);
+      }
+    } else {
+      console.log('trying green icon')
+
+      return {
+        icon: greenIcon
+      }
+    }
+  }
+  let seniorCenterGeojsonOptions = {
+    style: centerStyle,
+    onEachFeature: createPopup
+  }
+  centers = L.geoJSON(data, seniorCenterGeojsonOptions).addTo(map)
+  // L.marker(data, {icon: greenIcon}).toGeoJSON().addTo(map)
+})
+
+let createPopup = function (feature, layer) {
+  let name = feature.properties.NAME
+  let address = feature.properties.FULL_ADDRESS
+  let city = feature.properties.CITY
+  let state = feature.properties.STATE
+  let zip = feature.properties.ZIP
+  layer.bindPopup('<b>Senior Center Name: </b>' + name + '<br><br>' + '<b> Address: </b>' + address + ', ' + city + ', ' + state + ', ' + zip)
 }
