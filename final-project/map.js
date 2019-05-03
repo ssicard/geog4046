8332f01f528d78a8f1b0cb52ed8bc0da1b6c79bd
Sarah Sicard <sarah.sicard2@gmail.com>
2019-05-02 22:13:09 -0500

    finished project

--- final-project/map.js
+++ final-project/map.js
@@ -1,13 +1,21 @@
 // SETTING GLOBAL VARS //
-var brCenterCol = "#28502E";
-var noBrCenterCol = "#1B2F33";
+var brCenterCol = "#2b176d";
+var noBrCenterCol = "#547aa5";
 let seniorCentersUrl = 'https://opendata.arcgis.com/datasets/ca5cc72995064330ae19708f2cfaf134_0.geojson';
 
-let map = L.map('map').setView([30.5, -91.18], 10)
-L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-  maxZoom: 19
-}).addTo(map)
 
+let map = L.map('map').setView([30.5, -91.18], 10)
+
+// SETTING BASEMAP TOGGLE //
+let grayStreets = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png').addTo(map);
+let grayBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(map);
+let streetsBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(map);
+let basemaps = {
+  'Yellow Basemap, Streets': streetsBasemap,
+  'Gray Basemap': grayBasemap,
+  'Gray Basemap, Streets': grayStreets
+}
+L.control.layers(basemaps).addTo(map);
 
 $.getJSON(seniorCentersUrl, function(data) {
     var allCenters = L.geoJson(data);
@@ -22,7 +30,7 @@ $.getJSON(seniorCentersUrl, function(data) {
       let fillOpacity = .8; //more transparent
 
       if(city == 'BATON ROUGE'){
-        color = brCenterCol, //greenish
+        color = brCenterCol, //navy blue
         fillOpacity = 1   //more solid
       }
 
@@ -74,6 +82,7 @@ $.getJSON(seniorCentersUrl, function(data) {
 });
 
 let createPopup = function (feature, layer) {
+
   let name = feature.properties.NAME
   let address = feature.properties.FULL_ADDRESS
   let city = feature.properties.CITY
@@ -85,3 +94,17 @@ let createPopup = function (feature, layer) {
 let createMarker = function (feature, latlng) {
   return L.circleMarker(latlng);
 }
+
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create('div', 'info legend'),
+    grades = ["Baton Rouge Centers", "Non Baton Rouge Centers"],
+    labels = [brCenterCol, noBrCenterCol];
+
+  for(var i = 0; i < grades.length; i++){
+    div.innerHTML += '<i style="background:' + labels[i] + '"></i> ' + grades[i] + "<br/><br/>";
+  }
+
+  return div;
+}
+legend.addTo(map);
